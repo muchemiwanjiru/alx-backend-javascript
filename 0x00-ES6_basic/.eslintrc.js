4d10b0a8876a19367e879e820837bb2a47c34998
Wbsjael <muchemijessica77@gmail.com>
2024-10-01 19:58:41 +0000

    backened javascript

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,17 +1,48 @@
-module.exports = {
+
+#!/usr/bin/python3
+rts = {
   env: {
-    browser: true,
-    es2021: true,
-    node: true,
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  extends: ["eslint:recommended"],
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
-    ecmaVersion: 12,
-    sourceType: "module",
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: ['jest'],
   rules: {
-    "no-var": "error",
-    "prefer-const": "error",
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
   },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
 };
+""" Test .get() and .count() methods
+"""
+from models import storage
+from models.state import State
+
+print("All objects: {}".format(storage.count()))
+print("State objects: {}".format(storage.count(State)))
+
+first_state_id = list(storage.all(State).values())[0].id
+print("First state: {}".format(storage.get(State, first_state_id)))
 
