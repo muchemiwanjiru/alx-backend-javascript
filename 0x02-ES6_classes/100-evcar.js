03fd6352591b291a1d81d7306797ec1caad435b5
muchemiwanjiru <muchemiwanjiru1@gmail.com>
2024-10-04 07:07:56 +0800

    0x02-ES6_classes

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,22 +1,22 @@
+/* eslint no-underscore-dangle: 0 */ // ---> OFF
 import Car from './10-car';
 
+/* Represents EVCar object */
 export default class EVCar extends Car {
+  /**
+   * Creates new EVCar instance.
+   * @param {Brand} brand - Car's brand
+   * @param {Motor} motor - Car's motor type
+   * @param {Color} color - Car's color
+   * @param {Range} range - Car's range
+   */
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
-  }
-
-  cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+  /* Getter for @@species static property */
+  static get [Symbol.species]() {
+    return Car;
   }
 }
